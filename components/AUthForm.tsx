bf07df65c76856429e039b26e8d7896d025a7a27
Dipesh <dipesh@example.com>
2024-11-10 17:36:01 +0545

    integration of shadcn/ui

--- components/AUthForm.tsx
+++ components/AUthForm.tsx
@@ -1,12 +1,66 @@
-import React from 'react'
+"use client"
 
-type FormType ="sign-in" | 'sign-up'
-const AUthForm = ({type}:{type:FormType}) => {
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+type FormType = "sign-in" | "sign-up"
+
+const AuthForm = ({ type }: { type: FormType }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  // Corrected onSubmit definition as an arrow function
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <div>AuthForm
-      
-    </div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+        <h1 className="form-title">{type === "sign-in"? "Sign In": "Sign Up"}</h1>
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
-export default AUthForm
+export default AuthForm
