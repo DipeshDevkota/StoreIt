56d51820181f061cb83e983c3846d5f6bca68393
Dipesh <dipesh@example.com>
2024-11-11 14:27:23 +0545

    AuthForm Layout

--- components/AUthForm.tsx
+++ components/AUthForm.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import { z } from "zod"
+import Link from 'next/link';
+
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
- 
+import { Loader } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -15,7 +17,8 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-
+import { useState } from "react"
+import Image from "next/image"
 const formSchema = z.object({
   username: z.string().min(2).max(50),
 })
@@ -23,6 +26,9 @@ const formSchema = z.object({
 type FormType = "sign-in" | "sign-up"
 
 const AuthForm = ({ type }: { type: FormType }) => {
+
+     const [isLoading,setIsLoading] = useState(false);
+     const [errormessage,setErrormessage]= useState("")
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -38,28 +44,75 @@ const AuthForm = ({ type }: { type: FormType }) => {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
-        <h1 className="form-title">{type === "sign-in"? "Sign In": "Sign Up"}</h1>
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <>
+   <Form {...form}>
+  <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+    <h1 className="form-title">{type === "sign-in" ? "Sign In" : "Sign Up"}</h1>
+    {type === "sign-up" && (
+      <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem className="shad-form-item">
+            <div>
+            <FormLabel className="shad-form-label">Full Name</FormLabel>
+            
+              </div>
+            <FormControl>
+              <Input placeholder="enter your full name" {...field}  className="shad-input"/>
+            </FormControl>
+
+            <FormMessage className="shad-form-message" />
+          </FormItem>
+        )}
+      />
+
+      
+    )}
+     <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem className="shad-form-item">
+            <div>
+            <FormLabel className="shad-form-label">Email</FormLabel>
+            
+              </div>
+            <FormControl>
+              <Input placeholder="enter your email" {...field}  className="shad-input"/>
+            </FormControl>
+
+            <FormMessage className="shad-form-message" />
+          </FormItem>
+        )}
+      />
+<Button className="form-submit-button" type="submit" disabled={isLoading}>
+  {type === "sign-in" ? "Sign In" : "Sign Up"}
+  {isLoading && (
+    <Image 
+  src="/assets/icons/loader.svg" 
+  alt="loader"
+  width={24}
+  height={24}
+  className="ml-2 animate-spin"
+/>
+  )}
+</Button>
+{errormessage && <p className="error-message">*{errormessage}</p>}
+<div className="body-2 flex justify-center">
+  <p className="text-light-100">
+{type==="sign-in"?"Don't have an  account?":"Already have an account?"}
+</p>
+<Link href={type==="sign-in"?"/sign-up":"/sign-in"} className="ml-1 font-medium text-brand">
+{" "}
+{type ==="sign-in" ? "Sign-up":"Sign In"}</Link>
+</div>
+  </form>
+</Form>
+
+
+    {/* OTP Verification */}
+    </>
   )
 }
 
